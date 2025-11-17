dd4f70f9351a2530195512ff0f51eeddb325a9f4
hiyashinsu <esutarosa.git@gmail.com>
2025-11-17 02:18:20 +0200

    feat(web): use typography on home

--- packages/ui/src/pages/home.rs
+++ packages/ui/src/pages/home.rs
@@ -1,13 +1,24 @@
 use dioxus::prelude::*;
 
-use crate::shared::ui::button::Button;
+use crate::shared::ui::{
+    button::Button,
+    typography::{
+        Typography, TypographySize, TypographyTag, TypographyTransform, TypographyWeight,
+    },
+};
 
 #[component]
 pub fn HomePage() -> Element {
     rsx! {
         main {
             class: "page",
-            h1 { "amanogawax" }
+            Typography {
+                r#as: TypographyTag::H1,
+                size: TypographySize::Xl5,
+                weight: TypographyWeight::Regular,
+                transform: TypographyTransform::Capitalize,
+                "amanogawax"
+            }
             Button {}
         }
     }
